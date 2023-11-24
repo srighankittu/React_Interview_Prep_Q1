3757c1ae67b7e9fba8fe87426a3fc1e0448a1ac3
srighankittu <107845663+srighankittu@users.noreply.github.com>
2023-11-24 19:32:38 +0530

    pushing the changhes for this question

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import { Leftbox } from "./Components/Leftbox";
+// import { Rightbox } from "./Components/Rightbox";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [leftBoxItems, setLeftBoxItems] = useState([
+    "orange",
+    "apple",
+    "banana",
+    "pear",
+    "grapes",
+  ]);
+
+  const [rightBoxItems, setRightBoxItems] = useState([
+    "dog",
+    "cat",
+    "ant",
+    "goat",
+    "chicken",
+  ]);
+  let LeftTransportableItems = [];
+  let RightTransportableItems = [];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container">
+      <div className="box">
+        {leftBoxItems.map((item) => {
+          return (
+            <div>
+              <input
+                onChange={(e) => {
+                  if (e.target.checked) {
+                    LeftTransportableItems.push(item);
+                  } else if (!e.target.checked) {
+                    LeftTransportableItems.pop(item);
+                  }
+                  console.log(LeftTransportableItems);
+                }}
+                type="checkbox"
+                id="scales"
+                name={item}
+              />
+              <label htmlFor={item}>{item}</label>
+            </div>
+          );
+        })}
+      </div>
+      <div className="buttons">
+        <button
+          onClick={() => {
+            setRightBoxItems(rightBoxItems.concat(LeftTransportableItems));
+            setLeftBoxItems(
+              leftBoxItems.filter(
+                (item) => !LeftTransportableItems.includes(item)
+              )
+            );
+            LeftTransportableItems = [];
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Right
+        </button>
+        <button
+          onClick={() => {
+            setLeftBoxItems(leftBoxItems.concat(RightTransportableItems));
+            setRightBoxItems(
+              rightBoxItems.filter(
+                (item) => !RightTransportableItems.includes(item)
+              )
+            );
+            RightTransportableItems = [];
+          }}
+        >
+          Left
+        </button>
+      </div>
+      <div className="box">
+        {rightBoxItems.map((item) => {
+          return (
+            <div>
+              <input
+                onChange={(e) => {
+                  if (e.target.checked) {
+                    if (e.target.checked) {
+                      RightTransportableItems.push(item);
+                    } else if (!e.target.checked) {
+                      RightTransportableItems.pop(item);
+                    }
+                    console.log(RightTransportableItems);
+                  }
+                }}
+                type="checkbox"
+                id="scales"
+                name={item}
+              />
+              <label htmlFor={item}>{item}</label>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
